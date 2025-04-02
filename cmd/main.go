c9cd09e3a1872664eff203d5c248df76ae6ffffd
dmussina <dmussina@gmail.com>
2025-04-02 17:48:04 +0500

    cookies

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, world!")
 	})
+	http.HandleFunc("/set-cookie", setCookieHandler)
+	http.HandleFunc("/get-cookie", getCookieHandler)
 
 	addr := fmt.Sprintf(":%d", *port)
 	log.Printf("Server is starting on host %s...", addr)
@@ -31,4 +33,28 @@ func main() {
 	if err != nil {
 		log.Fatalf("Starting server error: %v", err)
 	}
+}
+
+// установка кук
+func setCookieHandler(w http.ResponseWriter, r *http.Request) {
+	cookie := &http.Cookie{
+		Name:     "session_id",
+		Value:    "123456",
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+	http.SetCookie(w, cookie)
+	w.Write([]byte("Cookie set!"))
+}
+
+// чтение кук
+func getCookieHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		http.Error(w, "Cookie not found", http.StatusNotFound)
+		return
+	}
+	w.Write([]byte("Cookie value: " + cookie.Value))
 }
